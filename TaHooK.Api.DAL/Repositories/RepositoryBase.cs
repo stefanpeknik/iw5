71f7199ba478cebf633763e6c8a8117b51ae1e8f
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-09-28 13:53:57 +0200

    create repositories

---
+++ TaHooK.Api.DAL/Repositories/RepositoryBase.cs
@@ -0,0 +1,45 @@
+using TaHooK.Api.DAL.Common.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaHooK.Api.DAL.Repositories;
+
+public class RepositoryBase<TEntity>: IRepository<TEntity> where TEntity : class, IEntity
+{
+    private readonly DbContext _dbContext;
+    
+    public RepositoryBase(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public IQueryable<TEntity> GetAll()
+    {
+        return _dbContext.Set<TEntity>();
+    }
+    
+    public async Task<TEntity?> GetByIdAsync(Guid id)
+    {
+        var entity = await _dbContext.Set<TEntity>().FindAsync(id);
+        return entity;
+    }
+    
+    public async Task<Guid> InsertAsync(TEntity entity)
+    {
+        var createdEntity = await _dbContext.Set<TEntity>().AddAsync(entity);
+        return createdEntity.Entity.Id;
+    }
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        TEntity existingEntity = await _dbContext.Set<TEntity>().SingleAsync(e => e.Id == entity.Id);
+        // add mapping
+        return existingEntity;
+    }
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        var entity = await _dbContext.Set<TEntity>().FindAsync(id);
+        _dbContext.Set<TEntity>().Remove(entity);
+    }
+
+}
