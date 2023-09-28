d5b61f53a2e64c33094311ebcf48477502a26f20
Večerka Lukáš (242168) <xvecer30@vutbr.cz>
2023-09-28 12:28:47 +0000

    Merged PR 3: Setup db for local development, implemented db context and repositories
    Related work items: #10, #6

---
+++ TaHooK.Api.DAL/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+using TaHooK.Api.DAL.Common.Entities.Interfaces;
+
+namespace TaHooK.Api.DAL.Repositories;
+
+public interface IRepository<TEntity> where TEntity : IEntity
+{
+    IQueryable<TEntity> GetAll();
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task<Guid> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task DeleteAsync(Guid id);
+}
---
+++ TaHooK.Api.DAL/Repositories/RepositoryBase.cs
@@ -0,0 +1,49 @@
+using TaHooK.Api.DAL.Common.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaHooK.Api.DAL.Repositories;
+
+public class RepositoryBase<TEntity>: IRepository<TEntity> where TEntity : class, IEntity
+{
+    private readonly DbContext _dbContext;
+    
+    public RepositoryBase(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public IQueryable<TEntity> GetAll()
+    {
+        return _dbContext.Set<TEntity>();
+    }
+    
+    public async Task<TEntity?> GetByIdAsync(Guid id)
+    {
+        var entity = await _dbContext.Set<TEntity>().FindAsync(id);
+        return entity;
+    }
+    
+    public async Task<Guid> InsertAsync(TEntity entity)
+    {
+        var createdEntity = await _dbContext.Set<TEntity>().AddAsync(entity);
+        return createdEntity.Entity.Id;
+    }
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        TEntity existingEntity = await _dbContext.Set<TEntity>().SingleAsync(e => e.Id == entity.Id);
+        //TODO: add mapping
+        return existingEntity;
+    }
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        var entity = await _dbContext.Set<TEntity>().FindAsync(id);
+
+        if (entity != null)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);    
+        }
+    }
+
+}
