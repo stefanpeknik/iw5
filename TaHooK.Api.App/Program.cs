7e49186c8be9c720678d2dec7ec14d653cac0031
JachymD <jachym.dolezal@gmail.com>
2023-10-17 16:36:26 +0200

    ref Program.cs

--- TaHooK.Api.App/Program.cs
+++ TaHooK.Api.App/Program.cs
@@ -8,19 +8,26 @@ using TaHooK.Api.DAL.Installers;
 //using TaHooK.Common.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+ConfigureDependencies(builder.Services, builder.Configuration);
+ConfigureAutoMapper(builder.Services);
+
+
 var logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
     .CreateLogger();
+
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+ValidateAutoMapperConfiguration(app.Services);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -29,10 +36,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Add services to the container.
-ConfigureDependencies(builder.Services, builder.Configuration);
-ConfigureAutoMapper(builder.Services);
-ValidateAutoMapperConfiguration(app.Services);
+
+// Migrate database
+using var scope = app.Services.CreateScope();
+if (app.Environment.IsDevelopment())
+{
+    scope.ServiceProvider.GetRequiredService<IDbMigrator>().Migrate();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
 
 void ConfigureDependencies(IServiceCollection serviceCollection, IConfiguration configuration)
 {
@@ -51,19 +69,3 @@ void ValidateAutoMapperConfiguration(IServiceProvider serviceProvider)
     var mapper = serviceProvider.GetRequiredService<IMapper>();
     mapper.ConfigurationProvider.AssertConfigurationIsValid();
 }
-
-
-// Migrate database
-using var scope = app.Services.CreateScope();
-if (app.Environment.IsDevelopment())
-{
-    scope.ServiceProvider.GetRequiredService<IDbMigrator>().Migrate();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
