46fb48aec1c5a03245c5d2c36979150e59c38da7
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-10-17 17:29:41 +0200

    implemented answer repo integration tests

--- TaHooK.Api.DAL.Tests/IntegrationTests/AnswerRepositoryTests.cs
+++ TaHooK.Api.DAL.Tests/IntegrationTests/AnswerRepositoryTests.cs
@@ -10,29 +10,94 @@ namespace TaHooK.Api.DAL.Tests.IntegrationTests;
 public class AnswerRepositoryTests: DALTestsBase
 {
     public AnswerRepositoryTests(ITestOutputHelper output) : base(output) { }
+
+    [Fact]
+    public void GetAll_Answers()
+    {
+        // Arrange
+        var repository = UnitOfWork.GetRepository<AnswerEntity>();
+        
+        // Act
+        var result = repository.Get();
+        
+        // Assert
+        Assert.True(result.Contains(AnswerSeeds.DefaultAnswer));
+        Assert.True(result.Contains(AnswerSeeds.AnswerUnderQuestionToDelete));
+    }
+    
+    [Fact]
+    public async Task Exists_Answer_True()
+    {
+        // Arrange
+        var repository = UnitOfWork.GetRepository<AnswerEntity>();
+        
+        // Act
+        var result = await repository.ExistsAsync(AnswerSeeds.DefaultAnswer);
+        
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public async Task Exists_Answer_False()
+    {
+        // Arrange
+        var repository = UnitOfWork.GetRepository<AnswerEntity>();
+        var answer = AnswerSeeds.DefaultAnswer with { Id = Guid.NewGuid() };
+        
+        
+        // Act
+        var result = await repository.ExistsAsync(answer);
+        
+        // Assert
+        Assert.False(result);
+    }
     
     [Fact]
     public async Task InsertNew_Answer()
     {
         // Arrange
         var repository = UnitOfWork.GetRepository<AnswerEntity>();
-        var entity = new AnswerEntity()
-        {
-            Id = Guid.NewGuid(),
-            IsCorrect = true,
-            Picture = null,
-            Question = null!,
-            QuestionId = QuestionSeeds.DefaultQuestion.Id,
-            Text = "Newly added question",
-            Type = default
-        };
+        var newEntity = AnswerSeeds.DefaultAnswer with { Id = Guid.NewGuid() }; 
 
         // Act
-        var insertedEntity = await repository.InsertAsync(entity);
+        var insertedEntity = await repository.InsertAsync(newEntity);
         await UnitOfWork.CommitAsync();
 
         // Assert
         var retrieved = await DbContextInstance.Answers.FindAsync(insertedEntity.Id);
-        Assert.True(retrieved.IsDeepEqual(entity));
+        Assert.Equal(newEntity, retrieved);
+    }
+    
+    [Fact]
+    public async Task Update_Answer()
+    {
+        // Arrange
+        var repository = UnitOfWork.GetRepository<AnswerEntity>();
+        var updated = AnswerSeeds.DefaultAnswer with { Text = "Updated" };
+
+        // Act
+        await repository.UpdateAsync(updated);
+        await UnitOfWork.CommitAsync();
+
+        // Assert
+        var contains = await DbContextInstance.Answers.ContainsAsync(updated);
+        Assert.True(contains);
+    }
+    
+    [Fact]
+    public async Task Delete_Answer()
+    {
+        // Arrange
+        var repository = UnitOfWork.GetRepository<AnswerEntity>();
+        var answerToDelete = AnswerSeeds.AnswerUnderQuestionToDelete;
+
+        // Act
+        await repository.DeleteAsync(answerToDelete.Id);
+        await UnitOfWork.CommitAsync();
+
+        // Assert
+        var contains = await DbContextInstance.Answers.ContainsAsync(answerToDelete);
+        Assert.False(contains);
     }
 }
