5ed3431db1ded90e68a64abb6ee0e84eb3ff644b
stefanpeknik <stefa9982@seznam.cz>
2023-10-28 22:11:39 +0200

    chore(9):weird stuff

---
+++ TaHooK.Api.App.EndToEndTests/EndToEndTests/AnswerControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+using TaHooK.Common.Enums;
+using TaHooK.Common.Models.Answer;
+using Xunit;
+
+namespace TaHooK.Api.App.EndToEndTests.EndToEndTests;
+
+public class AnswerControllerTests : EndToEndTestsBase
+{
+
+    [Fact]
+    public async Task GetAllAnswers_Returns_At_Last_One_Answer()
+    {
+        // Act
+        var response = await client.Value.GetAsync("/api/answers");
+        response.EnsureSuccessStatusCode();
+        var answers = await response.Content.ReadFromJsonAsync<ICollection<AnswerListModel>>();
+        
+        // Assert
+        Assert.NotNull(answers);
+        Assert.NotEmpty(answers);
+    }
+    
+
+}
---
+++ TaHooK.Api.App.EndToEndTests/EndToEndTests/DummyControllerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace TaHooK.Api.App.EndToEndTests.EndToEndTests;
+
+public class DummyControllerTests : EndToEndTestsBase, IAsyncDisposable
+{
+    private readonly TaHooKApiApplicationFactory application;
+    private readonly Lazy<HttpClient> client;
+
+    public DummyControllerTests(TaHooKApiApplicationFactory application, Lazy<HttpClient> client)
+    {
+        this.application = application;
+        this.client = client;
+    }
+
+    public new async ValueTask DisposeAsync()
+    {
+        await application.DisposeAsync();
+        await base.DisposeAsync();
+    }
+}
