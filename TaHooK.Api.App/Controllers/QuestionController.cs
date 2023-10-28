6440a74c09396c02bcf9999e3a81e67bea377dc9
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-10-28 17:13:01 +0200

    clean

--- TaHooK.Api.App/Controllers/QuestionController.cs
+++ TaHooK.Api.App/Controllers/QuestionController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TaHooK.Api.BL.Facades;
 using TaHooK.Common.Models.Question;
 
