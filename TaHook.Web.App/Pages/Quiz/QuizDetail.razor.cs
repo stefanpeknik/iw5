bd92c1d46a6ae64e2a0d744a375c523d7f36b7c7
Matěj Toul <mates097@seznam.cz>
2023-12-05 13:46:20 +0100

    Improved the file structure

---
+++ TaHook.Web.App/Pages/Quiz/QuizDetail.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TaHook.Web.App.Pages.Quiz
+{
+    public partial class QuizDetail
+    {
+        [Parameter]
+        public int? Id { get; set; } //TODO: guid
+
+        public QuizDetailModel data = new("Title", DateTime.Today, false);
+
+        public record QuizDetailModel(string Title, DateTime Schedule, bool Finished);
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+            await base.OnInitializedAsync();
+        }
+
+        private async Task LoadData()
+        {
+            //TODO: Load data from facade
+        }
+    }
+}
