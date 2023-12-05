10c0aac5996db98eccb8b575eee6f320a2a91677
JachymD <jachym.dolezal@gmail.com>
2023-12-05 23:54:04 +0100

    add QuizApiClient and FacadeBase for Web.BL

---
+++ TaHooK.Web.BL/Facades/FacadeBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TaHooK.Common;
+
+namespace TaHooK.Web.BL.Facades;
+
+public abstract class FacadeBase<TDetailModel,TListModel>
+{
+    protected virtual string apiVersion => "3";
+    protected virtual string culture => CultureInfo.DefaultThreadCurrentCulture?.Name ?? "cs";
+    
+    protected  FacadeBase () {}
+
+
+    public virtual List<TListModel> GetAll()
+    {
+        var itemsAll = new List<TListModel>();
+        
+        return itemsAll;
+    }
+
+    public abstract Task<TDetailModel> GetByIdAsync(Guid id);
+
+    public virtual async Task SaveAsync(TDetailModel data)
+    {
+        try
+        {
+            await SaveToApiAsync(data);
+        }
+        catch (HttpRequestException exception) when (exception.Message.Contains("Failed to fetch"))
+        {
+        }
+    }
+
+    protected abstract Task<Guid> SaveToApiAsync(TDetailModel data);
+
+    public abstract Task DeleteAsync(Guid id);
+    
+
+}
