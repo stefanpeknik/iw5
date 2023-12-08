7685f4c7270dddd67d2617f1b9237ce5632ae4bf
JachymD <jachym.dolezal@gmail.com>
2023-12-08 13:54:09 +0100

    add nswag conf and FE facades to connect API with FE

--- TaHooK.Web.BL/Facades/QuizFacade.cs
+++ TaHooK.Web.BL/Facades/QuizFacade.cs
@@ -1,6 +1,25 @@
-﻿namespace TaHooK.Web.BL.Facades;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using TaHooK.Common.Models;
+using Microsoft.Extensions.Options;
+using TaHooK.Common.Models.Quiz;
 
-public class QuizFacade
-{
-    
-}
+namespace TaHooK.Web.BL.Facades;
+
+// public class QuizFacade : FacadeBase<QuizDetailModel, QuizListModel>
+// {
+//     private readonly IQuizApiClient apiClient;
+//     
+//     
+//     public QuizFacade(
+//         IQuizApiClient apiClient,
+//         QuizRepository QuizRepository,
+//         IMapper mapper,
+//         IOptions<LocalDbOptions> localDbOptions)
+//         : base(QuizRepository, mapper, localDbOptions)
+//     {
+//         this.apiClient = apiClient;
+//     }
+// }
--- TaHooK.Web.BL/Installers/WebBLInstaller.cs
+++ TaHooK.Web.BL/Installers/WebBLInstaller.cs
@@ -1,6 +1,31 @@
-﻿namespace TaHooK.Web.BL.Installers;
+﻿using System;
+using System.Net.Http;
+using TaHooK.Api.BL;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TaHooK.Web.BL.Installers;
 
 public class WebBLInstaller
 {
+    public void Install(IServiceCollection serviceCollection, string apiBaseUrl)
+    {
+        serviceCollection.AddTransient<IQuizApiClient, QuizApiClient>(provider =>
+        {
+            var client = CreateApiHttpClient(provider, apiBaseUrl);
+            return new QuizApiClient(client, apiBaseUrl);
+        });
+        
+        serviceCollection.Scan(selector =>
+            selector.FromAssemblyOf<WebBLInstaller>()
+                .AddClasses(classes => classes.AssignableTo<IAppFacade>())
+                .AsSelfWithInterfaces()
+                .WithTransientLifetime());
+    }
     
+    public HttpClient CreateApiHttpClient(IServiceProvider serviceProvider, string apiBaseUrl)
+    {
+        var client = new HttpClient() { BaseAddress = new Uri(apiBaseUrl) };
+        client.BaseAddress = new Uri(apiBaseUrl);
+        return client;
+    }
 }
