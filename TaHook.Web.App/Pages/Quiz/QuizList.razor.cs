c17d92384b27181dd0dc8c00ad3dc0abdae321c3
Matěj Toul <mates097@seznam.cz>
2023-12-11 13:44:01 +0100

    Added list views for Quiz games and templates

---
+++ TaHook.Web.App/Pages/Quiz/QuizList.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TaHooK.Common.Models.Question;
+using TaHooK.Common.Models.Quiz;
+using TaHooK.Web.BL.Facades;
+
+namespace TaHook.Web.App.Pages.Quiz
+{
+    public partial class QuizList
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+
+        [Inject] private NavigationManager? Navigation { get; set; }
+        [Inject] private QuizFacade? Facade { get; set; }
+
+        List<QuizListModel>? QuizGames { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            QuizGames = await Facade!.GetAllAsync();
+            await base.OnInitializedAsync();
+        }
+
+        protected void OnJoinQuiz(Guid quizGameId)
+        {
+            Navigation!.NavigateTo($"/lobby/{quizGameId}");
+        }
+    }
+}
---
+++ TaHook.Web.App/Pages/Quiz/QuizTemplateList.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TaHooK.Common.Models.Question;
+using TaHooK.Common.Models.Quiz;
+using TaHooK.Web.BL.Facades;
+
+namespace TaHook.Web.App.Pages.Quiz
+{
+    public partial class QuizTemplateList
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+
+        [Inject] private NavigationManager? Navigation { get; set; }
+        [Inject] private QuizTemplateFacade? Facade { get; set; }
+
+        List<QuizTemplateListModel>? QuizTemplates { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            QuizTemplates = await Facade!.GetAllAsync();
+            await base.OnInitializedAsync();
+        }
+
+        protected void OnShowDetail(Guid quizId)
+        {
+            Navigation!.NavigateTo($"/quiz/{quizId}");
+        }
+    }
+}
