cbf7e05c3e9fa83dbdcc9199fd3203b99b72e055
JachymD <jachym.dolezal@gmail.com>
2023-12-11 22:53:09 +0100

    add facades for Question and QuizTemplateFacade Post

--- TaHooK.Web.BL/Facades/QuestionFacade.cs
+++ TaHooK.Web.BL/Facades/QuestionFacade.cs
@@ -9,7 +9,40 @@ using TaHooK.Common.Models.Responses;
 
 namespace TaHooK.Web.BL.Facades;
 
-public class QuestionFacade
+public class QuestionFacade : IWebAppFacade
 {
+    private readonly IQuestionApiClient _apiClient;
+    
+    public QuestionFacade(IQuestionApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+    
+    public async Task<List<QuestionListModel>> GetAllAsync()
+    {
+        return (await _apiClient.QuestionsGetAsync()).ToList();
+    }
+    
+    public async Task<QuestionDetailModel> GetByIdAsync(Guid id)
+    {
+        return await _apiClient.QuestionsGetAsync(id);
+    }
+    
+    public async Task<IdModel> CreateQuestionAsync(QuestionCreateUpdateModel model)
+    {
+        return await _apiClient.QuestionsPostAsync(model);
+    }
+    
+    public async Task<IdModel> UpdateQuestionAsync(Guid questionId, string questionText, Guid templateId)
+    {
+        
+        var model = new QuestionCreateUpdateModel
+        {
+            Text = questionText,
+            QuizTemplateId = templateId
+        };
+        
+        return await _apiClient.QuestionsPutAsync(questionId, model);
+    }
     
 }
